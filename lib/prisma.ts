5623b02944733738b193ddf3e0cd4fbbc044b0cf
shironetsu <70092342+shironetsu@users.noreply.github.com>
2023-02-19 19:27:40 +0900

    Make global prisma type-safe

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,15 +3,14 @@ import { PrismaClient } from "@prisma/client";
 // Docs about instantiating `PrismaClient` with Next.js:
 // https://pris.ly/d/help/next-js-best-practices
 
-let prisma: PrismaClient;
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma;
